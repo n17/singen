d4bac3b65795b4f6cf50d751e1fe67d9faa41395
Oskars Zālītis <oskars@zalitis.net>
2022-08-10 17:32:31 +0300

    [IMP] Added some more unit tests

--- tests/test_singen.py
+++ tests/test_singen.py
@@ -9,4 +9,12 @@ def test_loka_3_single_values():
     assert singen(3, 0, 2, 1, 5, 10, 0) == pytest.approx(-0.866025)
     assert singen(3, 0, 1, 1, 5, 10, 0) == pytest.approx(0.866025)
 
+def test_getting_zeros():
+    assert [
+        singen(3, t, 3, 1, 0, 10, 0) for t in range(10)
+    ] == pytest.approx([0,0,0,0,0,0,0,0,0,0])
 
+def test_getting_ones():
+    assert [
+        singen(3, t, 3, 1, 0, 10, 0.5) for t in range(10)
+    ] == pytest.approx([1,1,1,1,1,1,1,1,1,1])
