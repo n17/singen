ece975da9aeac26d7e63b20e780a90d6315edf4f
Oskars Zālītis <oskars@zalitis.net>
2022-12-02 00:02:17 +0200

    [IMP] Refactored code, allowed "alternative > loka"
    The singen function can now be called without any
    arguments!

--- singen.py
+++ singen.py
@@ -1,15 +1,15 @@
 import numpy as np
 
 def singen(
-        loka: int,
-        time: int,
-        alternative: int,
-        amplitude: float,
-        frequency: float,  # Hz
-        sampling_rate: int,  # Hz
+        time: int = 0,
+        frequency: float = 110,  # Hz
+        sampling_rate: int = 30,  # Hz
+        loka: int = 1,
+        alternative: int = 1,
+        amplitude: float = 1,
         phase_offset: float = 0,  # radians
 ):
-    assert 0 < alternative <= loka
+    assert 0 < alternative
     total_phase_offset = np.pi * (alternative / loka * 2 + phase_offset)
     return amplitude * np.sin(
         2 * np.pi * frequency * time / sampling_rate + total_phase_offset
--- tests/test_singen.py
+++ tests/test_singen.py
@@ -3,18 +3,48 @@ sys.path.append(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))
 ))
 from singen import singen
+from math import pi
 
 def test_loka_3_single_values():
-    assert singen(3, 0, 3, 1, 5, 10, 0) == pytest.approx(0)
-    assert singen(3, 0, 2, 1, 5, 10, 0) == pytest.approx(-0.866025)
-    assert singen(3, 0, 1, 1, 5, 10, 0) == pytest.approx(0.866025)
+    assert singen(
+        loka = 3,
+        alternative = 3,
+    ) == pytest.approx(0)
+    assert singen(
+        loka = 3,
+        alternative = 2,
+    ) == pytest.approx(-0.866025)
+    assert singen(
+        loka = 3,
+        alternative = 1,
+    ) == pytest.approx(0.866025)
+    assert singen(
+        loka = 3,
+        alternative = 4,
+    ) == pytest.approx(0.866025)
 
 def test_getting_zeros():
-    assert [
-        singen(3, t, 3, 1, 0, 10, 0) for t in range(10)
-    ] == pytest.approx([0,0,0,0,0,0,0,0,0,0])
+    test_list = [
+        singen(
+            time = t,
+            frequency = 0,
+        ) for t in range(10)
+    ]
+    assert test_list == pytest.approx(
+        [0,0,0,0,0,0,0,0,0,0]
+    )
 
 def test_getting_ones():
-    assert [
-        singen(3, t, 3, 1, 0, 10, 0.5) for t in range(10)
-    ] == pytest.approx([1,1,1,1,1,1,1,1,1,1])
+    test_list = [
+        singen(
+            time = t,
+            frequency = 0,
+            phase_offset = 0.5,
+        ) for t in range(10)
+    ]
+    assert test_list == pytest.approx(
+        [1,1,1,1,1,1,1,1,1,1]
+    )
+
+def test_no_arguments():
+    assert singen() == pytest.approx(0)
